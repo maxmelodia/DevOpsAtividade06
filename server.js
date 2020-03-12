6d446b3a91bba9b71d4a4ab7f790e5add94cc813
Maxwell Roberto Duarte <1054709@sga.pucminas.br>
2020-03-12 20:48:07 -0300

    Add master.

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next){
+ res.setHeader("Access-Control-Allow-Origin", "*");
+ res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+ res.setHeader("Access-Control-Allow-Headers", "content-type");
+ res.setHeader("Content-Type", "application/json");
+ res.setHeader("Access-Control-Allow-Credentials", true);
+ next();
+});
+
+app.listen(9090, function(){ console.log('Servidor Web rodando na porta 9090') }); 
+
+app.get('/api', function(req, res){
+    var response = {status: 'sucesso', resultado: 'ok'};
+     res.json(response);
+   });
